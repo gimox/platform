d48bb35dd689f595d1cb6cd96c09d625204aa4f8
Giorgio Modoni <modogio@gmail.com>
2024-10-28 06:52:46 +0100

    ad character

--- Scripts/character.gd
+++ Scripts/character.gd
@@ -8,6 +8,7 @@ extends CharacterBody2D
 @export_category("Jump")
 @export var _jump_height : float = 2.5
 @export var _air_control : float = 0.5
+@export var _jump_dust  : PackedScene
 var _jump_velocity : float
 
 @onready var _sprite : Sprite2D = $Sprite2D
@@ -39,6 +40,7 @@ func run(direction : float):
 func jump():
 	if is_on_floor():
 		velocity.y = _jump_velocity
+		_spawn_dust(_jump_dust)
 		
 func stop_jump():
 	if velocity.y < 0:
@@ -75,5 +77,9 @@ func _air_physics(delta: float):
 		if _direction:
 				velocity.x = move_toward(velocity.x, _direction * _speed, _acceleration * _air_control * delta)
 
-
+func _spawn_dust(dust: PackedScene):
+	var _dust = dust.instantiate()
+	_dust.position = position
+	_dust.flip_h = _sprite.flip_h
+	get_parent().add_child(_dust)
 	
